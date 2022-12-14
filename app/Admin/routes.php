7f8b09bff1fceb67fbc435fe59fb8f939d965082
Heng Oudom <shinhan@gmail.com>
2022-12-14 16:53:30 +0700

    Update on button and interface new property indication

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -16,19 +16,22 @@ Admin::routes();
 
         // $router->get('/dfdsfdsfdsfsdffss', 'HomeController@index')->name('home');
         $router->resource('/', ApplicationController::class);
-        $router->resource('networks', NetworkController::class);
-        $router->resource('applications', ApplicationController::class);
-        $router->resource('mobileapps', MobileappController::class);
-		$router->resource('contract', ContractController::class);
-		$router->resource('ipt', IptController::class);
-		$router->resource('inventory', InventoryController::class);
-		$router->resource('task', TaskController::class);
-		$router->resource('history', HistoryController::class);
-		$router->resource('transfer', TransferController::class);
-		$router->resource('document', DocumentController::class); 
-		$router->resource('wifi', WifiController::class); 
-		$router->resource('vendor', VendorController::class); 
-		$router->resource('invoice', InvoiceController::class); 
+        // $router->resource('networks', NetworkController::class);
+        // $router->resource('applications', ApplicationController::class);
+        // $router->resource('mobileapps', MobileappController::class);
+		// $router->resource('contract', ContractController::class);
+		// $router->resource('ipt', IptController::class);
+		// $router->resource('inventory', InventoryController::class);
+		// $router->resource('task', TaskController::class);
+		// $router->resource('history', HistoryController::class);
+		// $router->resource('transfer', TransferController::class);
+		// $router->resource('document', DocumentController::class); 
+		// $router->resource('wifi', WifiController::class); 
+		// $router->resource('vendor', VendorController::class); 
+		// $router->resource('invoice', InvoiceController::class); 
+
+        $router->resource('/property_indicator',  PropertyIndicatorController::class);
+        
     });
 
 
