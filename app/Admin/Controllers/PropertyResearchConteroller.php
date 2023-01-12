2e2136f7f95d6238ce20006521575badae1c9e76
Shinhan Soheng <phoeurn.soheng@shinhan.com>
2023-01-12 17:17:28 +0700

    get latest

--- app/Admin/Controllers/PropertyResearchConteroller.php
+++ app/Admin/Controllers/PropertyResearchConteroller.php
@@ -325,31 +325,109 @@ class PropertyResearchConteroller extends AdminController
      * @return Show
      */
     protected function detail($id)
+    // {
+    //     $show = new Show( PropertyResearch::findOrFail($id));
+    //     $show->field('information_type', __('Information Type'));
+    //     $show->field('property_reference', __('Property Reference'));
+    //     $show->field('access_road_name', __('Access Road Name'));
+    //     $show->field('no_of_floor', __('No of Floor'));
+    //     $show->field('land_size', __('Land Size'));
+    //     $show->field('build_value_per_sqm', __('Build Value per Sqm'));
+    //     $show->field('district', __('District/Khan'));
+    //     $show->field('contact_no', __('Contact No.'));
+    //     $show->field('remark', __('Remark'));
+    //     $show->field('location_type', __('Location Type'));
+    //     $show->field('property_type', __('Property Type'));
+    //     $show->field('land_title_type', __('Land Title Type'));
+    //     $show->field('property_value_per_sqm', __('Property per Sqm'));
+    //     $show->field('property_market_value', __('Property Market Value'));
+    //     $show->field('commune', __('Commune/Sangkat'));
+    //     $show->field('altitude', __('Altitude'));
+    //     $show->field('type_of_access_road', __('Type of Access Road'));
+    //     $show->field('borey', __('Borey'));
+    //     $show->field('information_date', __('Information Date'));
+    //     $show->field('build_size', __('Building Size'));
+    //     $show->field('province', __('Province'));
+    //     $show->field('village', __('Village'));
+    //     $show->field('laltitude', __('Laltitude'));
+    //     return $show;
+    // }
     {
-        $show = new Show( PropertyResearch::findOrFail($id));
-        $show->field('information_type', __('Information Type'));
-        $show->field('property_reference', __('Property Reference'));
-        $show->field('access_road_name', __('Access Road Name'));
-        $show->field('no_of_floor', __('No of Floor'));
-        $show->field('land_size', __('Land Size'));
-        $show->field('build_value_per_sqm', __('Build Value per Sqm'));
-        $show->field('district', __('District/Khan'));
-        $show->field('contact_no', __('Contact No.'));
-        $show->field('remark', __('Remark'));
-        $show->field('location_type', __('Location Type'));
-        $show->field('property_type', __('Property Type'));
-        $show->field('land_title_type', __('Land Title Type'));
-        $show->field('property_value_per_sqm', __('Property per Sqm'));
-        $show->field('property_market_value', __('Property Market Value'));
-        $show->field('commune', __('Commune/Sangkat'));
-        $show->field('altitude', __('Altitude'));
-        $show->field('type_of_access_road', __('Type of Access Road'));
-        $show->field('borey', __('Borey'));
-        $show->field('information_date', __('Information Date'));
-        $show->field('build_size', __('Building Size'));
-        $show->field('province', __('Province'));
-        $show->field('village', __('Village'));
-        $show->field('laltitude', __('Laltitude'));
+        $show = new Show( PropertyIndicator::findOrFail($id));
+
+       
+            $show->field('property_reference',__('Reference'));
+            $show->field('collateral_owner',__('Collateral Owner '));
+             
+
+            
+            $show->field('region_id', __('Region'))->as(function($region){
+                $region = Region::where('id', $region)->first();
+                if($region == null) return '';
+                return $region->region_name ;
+            });
+            $show->field('branch_code',__('Branch'))->as(function($branch_code){
+                $branch = Branch::where('branch_code', $branch_code)->first();
+                if($branch == null) return '';
+                return '(' . $branch->branch_code . ')' . $branch->branch_name;
+            });
+    
+            $show->field('requested_date',__('Requested Date'));
+            if (User::isVerifierRole() || User::isApproverRole()){
+            $show->field('reported_date',__('Reported Date'));
+            }
+            $show->field('cif_no',__('CIF No.'));
+            $show->field('rm_name',__('RM Name'));
+            $show->field('telephone',__('Telephone'));
+            $show->field('information_type',__('Information Type'))->as(function($id){
+                $informationtype = InformationType::where('id', $id)->first();
+                return  $informationtype->information_type_name;
+            });
+            $show->field('location_type',__('Location Type'));
+            $show->field('type_of_access_road',__('Type of Access Road'));
+            $show->field('access_road_name',__('Access Road Name'));
+            $show->field('property_type',__('Property Type'))->as(function($id){
+                $propertytype = PropertyType::where('id', $id)->first();
+                return  $propertytype->property_type_name;
+            });
+            $show->field('building_status',__('Building Status (%)'));
+            $show->field('borey',__('Borey'))->as(function($id){
+                $borey = Borey::where('id', $id)->first();
+                return $borey->borey_name;
+            }) ;
+            $show->field('no_of_floor',__('No. of floor'));
+            $show->field('land_title_type',__('Land Titil'));
+            $show->field('land_title_no',__('Lang Title No'));
+            $show->field('land_size',__('Land Size'));
+            $show->field('land_value_per_sqm',__('Land Value per Sqm ($)'));
+            $show->field('building_size',__('Building Size ($)'));
+            $show->field('building_value_per_sqm',__('Building Value per Sqm ($)'));
+            $show->field('property_value',__('Property Value ($)'));
+   
+            $show->field('customer_name',__('Customer Name'));
+            $show->field('client_contact_no',__('Cliend Contact No'));
+            $show->field('province_id',__('Province'))->as(function($province_id){
+                $province = Province::where('id', $province_id)->first();
+                return $province->province_name;
+            });
+            $show->field('district_id',__('District/ Khan'))->as(function($district_id){
+                $district = District::where('id', $district_id)->first();
+                return $district->district_name;
+            });
+            $show->field('commune_id',__('Commune / Sangkat'))->as(function($comune_id){
+                $commune = Commune::where('id', $comune_id)->first();
+                return $commune->commune_name;
+            });
+            $show->field('village_id',__('Village'))->as(function($village_id){
+                $village = Village::where('id', $village_id)->first();
+                return $village->village_name   ;
+            });
+            $show->field('longtitude',__('Longtitude'));
+            $show->field('latitude',__('Latitude'));
+            $show->field('front_photo',__('Front Photo'));
+            
+            $show->field('remark',__('Remark'));
+            
         return $show;
     }
 
