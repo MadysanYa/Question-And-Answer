c8843407c576f47662884cce04e026db46563f06
Srey reach <sreyreachsem0911@gmail.com>
2023-01-31 16:05:25 +0700

    update indication

--- resources/views/map/filterMap.blade.php
+++ resources/views/map/filterMap.blade.php
@@ -21,7 +21,7 @@
                                     <span class="icheck">
                                         <label style="margin-right: 10px;margin-bottom: 0px;display: flex;">
                                             <div class="" aria-checked="false" aria-disabled="false">
-                                                <input type="radio" class="released" name="check_list" value="indication" {{request()->check_list == "indicator" ? 'checked' : "false" }}  style="margin-right: 10px;">
+                                                <input type="radio" class="released" name="check_list" value="indication" {{request()->check_list == "indication" ? 'checked' : "false" }}  style="margin-right: 10px;">
                                             </div>Properties Indication
                                         </label>
                                     </span>
