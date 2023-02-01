442d95ea0e0ef49e6566537fc8d548bcaecf9f51
Srey reach <sreyreachsem0911@gmail.com>
2023-02-01 15:39:31 +0700

    Set up risk indication

--- app/Admin/Controllers/RiskIndicatorController.php
+++ app/Admin/Controllers/RiskIndicatorController.php
@@ -4,13 +4,14 @@ namespace App\Admin\Controllers;
 
 
 
-use Encore\Admin\Controllers\AdminController;
-use App\Models\RiskIndicator;
+use App\Models\User;
+use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
-use Illuminate\Support\Facades\Request;
+use App\Models\RiskIndicator;
 use Encore\Admin\Layout\Content;
-use App\Models\User;
+use Illuminate\Support\Facades\Request;
+use Encore\Admin\Controllers\AdminController;
 
 
 class RiskIndicatorController extends AdminController 
@@ -26,17 +27,101 @@ class RiskIndicatorController extends AdminController
      *
      * @return Grid
      */
+
+    public function index(Content $RiskIndicator)
+    {
+        $RiskIndicator->header($this->title);
+        // $RiskIndicator->body($this->dashboard());
+        $RiskIndicator->body($this->grid());
+
+        //Map 
+        //Property Indication
+        $riskPropertys = RiskIndicator::get();
+        $locationArray = [];
+
+        // Latitude, Longtitude
+        foreach($riskPropertys as $risk){
+            $raskLocation = [$risk->title.','.$risk->latitude.','.$risk->longtitude];
+            $arr = explode(",", implode(" ", $raskLocation));
+            $locationArray[] = $arr;
+        }
+        $arryRiskProperty = $locationArray;
+        
+        // Labels on marker
+        foreach($riskPropertys as $value){
+            $label = $value->title;
+            $labelArray[] = $label;
+        }
+        $arrayLabel = $labelArray;
+
+        //Information property indicator
+        foreach($riskPropertys as $value){
+            $info = [
+                $value->latitude.','.
+                $value->longtitude.','.
+                $value->description
+            ];
+            $arrInfo = explode(",", implode(" ", $info));
+            $infoArray[] = $arrInfo;
+        }
+        $infoProperty = $infoArray;
+
+        $RiskIndicator->body(view('risk.riskIndication', [
+            'arryRiskProperty' => $arryRiskProperty,
+            'arrayLabel' => $arrayLabel,
+            'infoProperty' => $infoProperty,
+        ]));
+
+        return $RiskIndicator;
+    }
+
+    protected function dashboard(){
+    
+   
+        return $html;
+    }	
    
     
-     protected function grid()
-     {
+    protected function grid()
+    {
 
-     }
+    }
   
 
 
 
+    protected function form()
+    {
+        $form = new Form(new RiskIndicator());
+        $form->column(1/2, function ($form){
+            $form->text('title', __('Title'))->rules('required');
+            $form->text('latitude', __('Latitude'))->inputmask(['mask' => '99.999999'])->rules('required');
+            $form->text('longtitude', __('Longtitude'))->inputmask(['mask' => '999.999999'])->rules('required');
+            $form->textarea('description', __('Description'));
+        });
 
+        $form->footer(function ($footer) {
+
+            // disable reset btn
+            $footer->disableReset();
+            // disable `View` checkbox
+            $footer->disableViewCheck();
+            // disable `Continue editing` checkbox
+            $footer->disableEditingCheck();
+            // disable `Continue Creating` checkbox
+
+            
+            //$footer->disableCreatingCheck();
+            $footer->disableCreatingCheck();
+
+        
+            
+        
+        });
+
+
+        return $form;
+    }
 
     
        
--- app/Admin/Controllers/TitleIndicatorController.php
+++ app/Admin/Controllers/TitleIndicatorController.php
@@ -2,16 +2,24 @@
 
 namespace App\Admin\Controllers;
 
-
-
-use Encore\Admin\Controllers\AdminController;
-use App\Models\TitleIndicator;
-use App\Models\PropertyIndicator;
+use Auth;
+use App\Models\User;
+use App\Model\Invoice;
+use App\Models\Branch;
+use App\Models\Region;
+use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
-use Illuminate\Support\Facades\Request;
+use Encore\Admin\Admin;
+use Encore\Admin\Widgets\Table;
+use App\Models\PropertyResearch;
 use Encore\Admin\Layout\Content;
-use App\Models\User;
+use App\Models\MapPriceIndicator;
+use App\Models\PropertyAppraisal;
+use App\Models\PropertyIndicator;
+use Encore\Admin\Form\Field\Button;
+use Illuminate\Support\Facades\Request;
+use Encore\Admin\Controllers\AdminController;
 
 
 class TitleIndicatorController extends AdminController 
@@ -21,42 +29,226 @@ class TitleIndicatorController extends AdminController
      *
      * @var string
      */
-    protected $title = 'Title Indicator';
+    protected $title = 'Map Price Indicator';
      /**
      * Make a grid builder.
      *
      * @return Grid
      */
-      
-    
-     protected function grid()
-     {
+    public function index(Content $MapPriceIndicator){
+
+        $MapPriceIndicator->header($this->title);
+        // $MapPriceIndicator->body($this->dashboard());
+        $MapPriceIndicator->body($this->grid());
+
+        //Map 
+        //Property Indication
+        $propertys = PropertyIndicator::get();
+        $locationArray = [];
+
+        // Latitude, Longtitude
+        foreach($propertys as $property){
+            $location = [$property->land_title_type.'$'.','.$property->latitude.','.$property->longtitude];
+            $arr = explode(",", implode(" ", $location));
+            $locationArray[] = $arr;
+        }
+        $arryProperty = $locationArray;
         
-       
-        $grid = new Grid(new PropertyIndicator);
-        
-        return $grid;
-     }
-     
+        //Labels on marker
+        foreach($propertys as $value){
+            $label = "$".$value->land_title_type;
+            $labelArray[] = $label;
+        }
+        $arrayLabel = $labelArray;
+
+        //Information property indicator
+        foreach($propertys as $value){
+            $info = [
+                $value->latitude.','.
+                $value->longtitude.','.
+                $value->branch_code.','.
+                $value->property_reference.','.
+                $value->cif_no.','.
+                $value->rm_name.','.
+                $value->telephone.','.
+                $value->requested_date.','.
+                $value->reported_date.','.
+                $value->information_type.','.
+                $value->location_type.','.
+                $value->type_of_access_road.','.
+                $value->access_road_name.','.
+                $value->property_type.','.
+                $value->building_status.','.
+                $value->borey.','.
+                $value->no_of_floor.','.
+                $value->land_title_type.','.
+                $value->created_at->format('d-m-Y').','.
+                $value->land_size.','.
+                $value->land_value_per_sqm.','.
+                $value->building_size.','.
+                $value->building_value_per_sqm.','.
+                $value->property_value.','.
+                $value->client_contact_no
+            ];
+            $arrInfo = explode(",", implode(" ", $info));
+            $infoArray[] = $arrInfo;
+        }
+        $infoProperty = $infoArray;
+
+        //Property Research
+        $propertyResearch = PropertyResearch::get();
+        $proResearch = [];
+
+        $latLongProResearch = $this->latLongProResearch($propertyResearch);
+        $labelProResearch = $this->labelProResearch($propertyResearch);
+        $infoProResearch = $this->infoProResearch($propertyResearch);
+
+        //Property Appraisal
+        $propertyAppraisal = PropertyAppraisal::get();
+        $proResearch = [];
+
+        $latLongProAppraisal = $this->latLongProAppraisal($propertyAppraisal);
+        $labelProAppraisal = $this->labelProAppraisal($propertyAppraisal);
+        $infoProAppraisal = $this->infoProAppraisal($propertyAppraisal);
 
 
+        if(request()->check_list == 'indication'){
+            $MapPriceIndicator->body(view('map.googleMapIndication', [
+                'arryProperty' => $arryProperty,
+                'arrayLabel' => $arrayLabel,
+                'infoProperty' => $infoProperty,
+            ]));
+        }elseif (request()->check_list == 'research') {
+            $MapPriceIndicator->body(view('map.googleMapPropertyResearch', [
+                'latLongProResearch' => $latLongProResearch,
+                'labelProResearch' => $labelProResearch,
+                'infoProResearch' => $infoProResearch
+            ]));
+        }elseif (request()->check_list == 'appraisal') {
+            $MapPriceIndicator->body(view('map.googleMapAppraisal', [
+                'latLongProAppraisal' => $latLongProAppraisal,
+                'labelProAppraisal' => $labelProAppraisal,
+                'infoProAppraisal' => $infoProAppraisal
+            ]));
+        }
+        else{
+            $MapPriceIndicator->body(view('map.googleMapIndication', [
+                'arryProperty' => $arryProperty,
+                'arrayLabel' => $arrayLabel,
+                'infoProperty' => $infoProperty,
+            ]));
+        }
 
-  
-
-
-
-
-
+        return $MapPriceIndicator;
+    }
     
-       
+    public function latLongProResearch($propertyResearch)
+    {
+        foreach ($propertyResearch as $value) {
+            $proResearchs = [$value->land_title_type.'$'.','.$value->latitude.','.$value->longtitude];
+            $propertySearch = explode(",", implode(" ", $proResearchs));
+            $proResearchArray[] = $propertySearch;
+        }
+        return $arrayProResearch = $proResearchArray;
+    }
+
+    public function labelProResearch($propertyResearch)
+    {
+        foreach($propertyResearch as $value){
+            $labelProResearch = "$".$value->land_title_type;
+            $arrayLabelProResearch[] = $labelProResearch;
+        }
+        return $arrayLabelProResearch = $arrayLabelProResearch;
+    }
+
+    public function infoProResearch($propertyResearch)
+    {
+        foreach($propertyResearch as $value){
+            $info = [
+                $value->latitude.','.
+                $value->longtitude.','.
+                $value->information_type.','.
+                $value->property_reference.','.
+                $value->location_type.','.
+                $value->property_type.','.
+                $value->type_of_access_road.','.
+                $value->access_road_name.','.
+                $value->borey.','.
+                $value->no_of_floor.','.
+                $value->land_title_type.','.
+                $value->created_at->format('d-m-Y').','.
+                $value->land_size.','.
+                $value->land_value_per_sqm.','.
+                $value->building_size.','.
+                $value->building_value_per_sqm.','.
+                $value->property_market_value
+            ];
+            $arrInfoProResearch = explode(",", implode(" ", $info));
+            $arrayInfor[] = $arrInfoProResearch;
+        }
+        return $infoPropertyResearch = $arrayInfor;
+    }
+
+    public function latLongProAppraisal($propertyAppraisal)
+    {
+        foreach ($propertyAppraisal as $value) {
+            $proAppraisal = [$value->land_title_type.'$'.','.$value->latitude.','.$value->longtitude];
+            $properAppraisal = explode(",", implode(" ", $proAppraisal));
+            $proAppraisalArray[] = $properAppraisal;
+        }
+        return $arrayAppraisal = $proAppraisalArray;
+    }
+
+    public function labelProAppraisal($propertyAppraisal)
+    {
+        foreach($propertyAppraisal as $value){
+            $labelProAppraisal = "$".$value->land_title_type;
+            $arrayLabelProAppraisal[] = $labelProAppraisal;
+        }
+        return $arrayLabelProAppraisal = $arrayLabelProAppraisal;
+    }
+
+    public function infoProAppraisal($propertyAppraisal)
+    {
+        foreach($propertyAppraisal as $value){
             
-        
-       
-        
-       
+            $info = [
+                $value->latitude.','.
+                $value->longtitude.','.
+                $value->branch_code.','.
+                $value->property_reference.','.
+                $value->cif_no.','.
+                $value->rm_name.','.
+                $value->telephone.','.
+                $value->requested_date.','.
+                $value->reported_date.','.
+                $value->information_type.','.
+                $value->location_type.','.
+                $value->type_of_access_road.','.
+                $value->access_road_name.','.
+                $value->land_title_type.','.
+                $value->property_type.','.
+                $value->building_status.','.
+                $value->borey.','.
+                $value->no_of_floor.','.
+                $value->created_at->format('d-m-Y').','.
+                $value->land_size.','.
+                $value->land_value_per_sqm.','.
+                $value->building_size.','.
+                $value->building_value_per_sqm.','.
+                $value->property_value.','.
+                $value->customer_name.','.
+                $value->client_contact_no
+            ];
+            $arrInfoProAppraisal = explode(",", implode(" ", $info));
+            $arrayInfor[] = $arrInfoProAppraisal;
+        }
+        return $infoPropertyAppraisal = $arrayInfor;
+    }
+    
+    protected function grid()
+    {
 
-
-        
-   
+    }
 
 }
