d8fda6c5bb1e7e9c28298a9259d6449cf46fd1e2
Heng Oudom <shinhan@gmail.com>
2023-02-27 10:59:50 +0700

    Rename indicator, appraisal

--- app/Models/PropertyResearch.php
+++ app/Models/PropertyResearch.php
@@ -13,7 +13,7 @@ class PropertyResearch extends Model
 {
     use SoftDeletes;
     use DeleteByUserTrait;
-    
+
     /**
      * Relationship
      */
@@ -54,6 +54,11 @@ class PropertyResearch extends Model
         $this->attributes['longtitude'] = trim($value,"_");
     }
 
+    public function setContactNoAttribute($value)
+    {
+        $this->attributes['contact_no'] = trim($value,"_");
+    }
+
     /**
      * Accessor
      */
@@ -78,7 +83,7 @@ class PropertyResearch extends Model
     public function verified(){
 
     }
-    
+
     public function approved(){
 
     }
